7c4d84e9710aaf9ec1cf6a00b2befa7aba4f8adf
b-blackwell <grasslookssuspicious@gmail.com>
2025-02-25 00:22:26 -0500

    add sys commands

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 def main():
+    import sys
     from stats import get_num_words, get_book_text, character_check, sort_dict
 
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    text = get_book_text(sys.argv[1])
     num_words = get_num_words(text)
     character_counts = character_check(text)
     sorted_counts = sort_dict(character_counts)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
